4f23c6380accad9ee03a54269be63e47865fb1bc
Haochen-S <aiden1747960409@gmail.com>
2024-04-14 17:38:52 +1000

    passed 4

--- Problems/4_Median_of_Two_Sorted_Arrays.py
+++ Problems/4_Median_of_Two_Sorted_Arrays.py
@@ -1,5 +1,6 @@
 # 4. Median of Two Sorted Arrays
 
+import math
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -7,28 +8,42 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        # middleNumber = 
-        isFound = False
-        while not isFound:
-            
-            Solution.findMiddle(nums1)
+        nums1Len = len(nums1)
+        nums2Len = len(nums2)
+        totalLen = nums1Len + nums2Len
 
-        
-        
-        
-    def findMiddle(input_list):
-        middle = float(len(input_list)) / 2
-        if middle % 2 != 0:
-            return input_list[int(middle - .5)]
+        mergeNum = []
+        isEven = False
+        if (totalLen % 2 == 0):
+            i = totalLen / 2
+            isEven = True
         else:
-            return (input_list[int(middle)], input_list[int(middle-1)])
+            i = math.floor(totalLen / 2)
+        i = int(i)
+
+        counter = 0
+        nums1Index = 0
+        nums2Index = 0
+
+        while (counter <= i):
+            if (nums1Index >= nums1Len):
+                mergeNum.append(nums2[nums2Index])
+                nums2Index += 1
+            elif (nums2Index >= nums2Len):
+                mergeNum.append(nums1[nums1Index])
+                nums1Index += 1
+            elif (nums1[nums1Index] <= nums2[nums2Index]):
+                mergeNum.append(nums1[nums1Index])
+                nums1Index += 1
+            elif (nums1[nums1Index] > nums2[nums2Index]):
+                mergeNum.append(nums2[nums2Index])
+                nums2Index += 1
+
+            counter += 1
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        if (isEven):
+            return float(mergeNum[i] + mergeNum[i - 1]) / 2
+
+        return mergeNum[i]
+    
+print(Solution.findMedianSortedArrays(Solution, [1,2], [3,4]))
