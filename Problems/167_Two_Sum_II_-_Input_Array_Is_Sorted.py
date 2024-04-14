49ff4318af1b1488b464386e8180f24a40bd8a97
Haochen-S <aiden1747960409@gmail.com>
2024-04-15 09:37:05 +1000

    passed 167. TwoSum II

---
+++ Problems/167_Two_Sum_II_-_Input_Array_Is_Sorted.py
@@ -0,0 +1,28 @@
+# 167. Two Sum II - Input Array Is Sorted
+
+# DONE
+import math
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        minIndex = 0
+        maxIndex = len(numbers) - 1
+
+        while True:
+            
+            if (numbers[minIndex] + numbers[maxIndex] == target):
+                return [minIndex + 1, maxIndex + 1]
+            elif (numbers[minIndex] + numbers[maxIndex] > target):
+                maxIndex -= 1
+            else:
+                minIndex += 1
+
+
+
+
+ 
+ 
