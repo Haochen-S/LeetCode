4b5e48cc733a6a8cbcc3c22b08f109d34f4c7a22
Haochen-S <aiden1747960409@gmail.com>
2024-04-14 15:47:44 +1000

    passed 1848

--- Problems/1848_Minimum_Distance_to_the_Target_Element.py
+++ Problems/1848_Minimum_Distance_to_the_Target_Element.py
@@ -1,3 +1,5 @@
+# 1848. Minimum Distance to the Target Element
+
 class Solution(object):
     def getMinDistance(self, nums, target, start):
         """
@@ -6,8 +8,20 @@ class Solution(object):
         :type start: int
         :rtype: int
         """
-        
-
-
-
-Solution.getMinDistance()
+        ansIndex = None
+        i = 0
+        arrayLen = len(nums)
+        while (i < arrayLen):
+            if (nums[i] == target):
+                if (ansIndex == None):
+                    ansIndex = i
+                    continue
+                
+                # if (start <= 0):
+                #     return abs(ansIndex - start)
+                
+                if (abs(i - start) < abs(ansIndex - start)):
+                    ansIndex = i
+ 
+            i += 1
+        return abs(ansIndex - start)
