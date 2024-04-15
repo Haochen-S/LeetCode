555aa2acdf9cda7ea1e5d4dab06307b8a8e00a40
Haochen-S <aiden1747960409@gmail.com>
2024-04-15 11:52:05 +1000

    Passed 15. 3Sum

--- Problems/15_3Sum.py
+++ Problems/15_3Sum.py
@@ -6,8 +6,52 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
-        
-        
-print(1 in [1,2])
 
+        # sort the array
+        nums.sort()
+
+        # find the ans[]
+        ans = []
+        lastNum = None
+        length = len(nums)
+        i = 0
+
+        while (i < length - 2):
+            if (lastNum != nums[i] ):
+                lastNum = nums[i]
+                result = twoSum(nums[i+1:], -lastNum, nums[i])
+                if (result != []):
+                    ans += result
+
+            i += 1
+
+        return ans
+        
+# 167. Two Sum II - Input Array Is Sorted
+def twoSum(numbers, target, leadNum):
+    """
+    :type numbers: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    minIndex = 0
+    maxIndex = len(numbers) - 1
+    ans = []
+    lastNum = None
+    while minIndex < maxIndex:
+        
+        if (lastNum == numbers[minIndex]):
+            minIndex += 1
+            continue
+
+        if (numbers[minIndex] + numbers[maxIndex] == target):
+            ans.append([leadNum, numbers[minIndex], numbers[maxIndex]])
+            # next index can not be the same number, avoid duplicate
+            lastNum = numbers[minIndex]
+            minIndex += 1
+        elif (numbers[minIndex] + numbers[maxIndex] > target):
+            maxIndex -= 1
+        else:
+            minIndex += 1
+
+    return ans
