e6fc8aadebd25545f3f79543e266756ab56f6837
Haochen-S <aiden1747960409@gmail.com>
2024-04-18 22:51:33 +1000

    working on 463

---
+++ Problems/463_Island_Perimeter.py
@@ -0,0 +1,31 @@
+# 463. Island Perimeter
+
+
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        if ()
+        topLayer = []
+        targetLayer = []
+        botLayer = []
+
+        numOfLayer = len(grid)
+        i = 0
+        while (i < numOfLayer):
+            if (topLayer == []):
+                topLayer = grid[i]
+                continue
+            if (targetLayer == []):
+                targetLayer = grid[i]
+                continue
+
+            i += 1
+
+
+def numOfLandSurrending(grid, target):
+    numOfLandSurrending = 0
+
+    return numOfLandSurrending
