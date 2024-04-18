b4291cb4051c80c6123a34bf6b7b59f14345c37d
Haochen-S <aiden1747960409@gmail.com>
2024-04-18 23:20:11 +1000

    passed 463

--- Problems/463_Island_Perimeter.py
+++ Problems/463_Island_Perimeter.py
@@ -7,26 +7,47 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        if ():
-            return 0
-        topLayer = []
-        targetLayer = []
-        botLayer = []
-
-        numOfLayer = len(grid)
+        numOfLandSurrending = 0
+        numOfLayers = len(grid)
         i = 0
-        while (i < numOfLayer):
-            if (topLayer == []):
-                topLayer = grid[i]
-                continue
-            if (targetLayer == []):
-                targetLayer = grid[i]
-                continue
+        while (i < numOfLayers):
+            j = 0
+            numOfElements = len(grid[i])
+            while (j < numOfElements):
+                if (grid[i][j] != 1):
+                    j += 1
+                    continue
+
+                # if most top and most bot, add one
+                if (i == 0):
+                    numOfLandSurrending += 1
+                if (i == numOfLayers - 1):
+                    numOfLandSurrending += 1
+                
+                # most left or most right, add one
+                if (j == 0):
+                    numOfLandSurrending += 1
+                if (j == numOfElements - 1):
+                    numOfLandSurrending += 1
+            
+                if (j + 1 < numOfElements):
+                    if (grid[i][j + 1] != 1):
+                        numOfLandSurrending += 1
+                if (j - 1 >= 0):
+                    if (grid[i][j - 1] != 1):
+                        numOfLandSurrending += 1
+
+                if (i + 1 < numOfLayers):
+                    if (grid[i + 1][j] != 1):
+                        numOfLandSurrending += 1
+                if (i - 1 >= 0):
+                    if (grid[i - 1][j] != 1):
+                        numOfLandSurrending += 1
+        
+                j += 1
 
             i += 1
 
+        return numOfLandSurrending
 
-def numOfLandSurrending(grid, target):
-    numOfLandSurrending = 0
 
-    return numOfLandSurrending
