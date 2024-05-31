3b10bb0b30d4438dbfda3ef9d96f471d71efec7d
Haochen-S <aiden1747960409@gmail.com>
2024-05-31 11:20:34 +1000

    finished 1442

---
+++ Finished/Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR_1442.java
@@ -0,0 +1,36 @@
+// 1442. Count Triplets That Can Form Two Arrays of Equal XOR
+class Solution {
+  public int countTriplets(int[] arr) {
+    int ans = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+
+        int a = 0;
+        for (int i_ = i; i_ < j; i_++) {
+            a ^= arr[i_];
+        }
+
+        int b = 0;
+        for (int k = j; k < arr.length; k++) {
+          b ^= arr[k];
+          if (a == b) {
+            ans++;
+          }
+        }
+      }
+    }
+    return ans;
+  }
+}
+
+
+public class Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR_1442 {
+  public static void main(String[] args) {
+    int[] arr = {};
+
+    Solution s = new Solution();
+    
+    System.out.println(s.countTriplets(arr));
+  }
+}
