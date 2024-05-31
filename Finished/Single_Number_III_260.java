294db88cbb8d34b8d2eea0f4da1c01c3d38ecdfc
Haochen-S <aiden1747960409@gmail.com>
2024-05-31 12:18:57 +1000

    passed 260

---
+++ Finished/Single_Number_III_260.java
@@ -0,0 +1,46 @@
+// 260. Single Number III
+import java.util.Arrays;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        if (nums.length == 2) {
+            return nums;
+        }
+        Arrays.sort(nums);
+        ArrayList<Integer> ans = new ArrayList<>();
+        int counter = 0;
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                i++;
+            } else {
+                ans.add(nums[i]);
+                counter++;
+                if (counter == 2) {
+                    break;
+                }
+            }
+        }
+        if (counter != 2) {
+            ans.add(nums[nums.length - 1]);
+        }
+        int[] intArray = new int[ans.size()];
+
+        for (int i = 0; i < ans.size(); i++) {
+            intArray[i] = ans.get(i);
+        }
+        return intArray;
+    }
+}
+
+public class Single_Number_III_260 {
+    
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,3,4,6};
+    
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(s.singleNumber(arr)));
+    }
+}
