7b7c9119658da6cfcc5201d9dbc224845da53470
Haochen-S <aiden1747960409@gmail.com>
2024-08-06 09:15:15 +1000

    working..

--- Unfinished Problems/36_Valid_Sudoku.py
+++ Unfinished Problems/36_Valid_Sudoku.py
@@ -1,47 +1,59 @@
-# 36. Valid Sudoku
+# # 36. Valid Sudoku
 
 
-class Solution(object):
-    def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
-        if (not checkEachBox(board)):
-            return False
-        if (not checkEachColumn(board)):
-            return False
-        if (not checkEachRow(board)):
-            return False
+# class Solution(object):
+#     def isValidSudoku(self, board):
+#         """
+#         :type board: List[List[str]]
+#         :rtype: bool
+#         """
+#         if (not checkEachBox(board)):
+#             return False
+#         if (not checkEachColumn(board)):
+#             return False
+#         if (not checkEachRow(board)):
+#             return False
 
-        return True
+#         return True
 
-def checkEachBox(board):
-    nineList = []
-    row = 0
-    column = 0
+# def checkEachBox(board):
+#     nineList = []
+#     row = 0
+#     column = 0
 
-    i = 0
-    # 0 1 2
-    # 3 4 5
-    # 6 7 8
-    while (i < 9):
-        j = 0
-        nineList = []
-        while (j < 9):
-            nineList.append(board[row, column + j])
-            j += 1 
-        if (column < 6):
-            column += 3
-        else:
-            column = 0
-        i += 1
-    return True        
+#     i = 0
+#     # 0 1 2
+#     # 3 4 5
+#     # 6 7 8
+#     while (i < 9):
+#         j = 0
+#         nineList = []
+#         while (j < 9):
+#             nineList.append(board[row, column + j])
+#             j += 1 
+#         if (column < 6):
+#             column += 3
+#         else:
+#             column = 0
+#         i += 1
+#     return True        
 
-def checkEachColumn(board):
-    nineList = []
-    return True
+# def checkEachColumn(board):
+#     nineList = []
+#     return True
 
-def checkEachRow(board):
-    nineList = []
-    return True
+# def checkEachRow(board):
+#     nineList = []
+#     return True
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        HashMap<int[], Double> map = new HashMap<int[], Double>();
+        for (int[] p : points) {
+            map.put(p, Math.sqrt(p[0] * p[0] + p[1] * p[1]));
+        }
+        
+        sys.out.println(map.toString());
+        
+    }
+}
--- Unfinished Problems/Maximum_Score_From_Removing_Substrings_1717.java
+++ Unfinished Problems/Maximum_Score_From_Removing_Substrings_1717.java
@@ -1,18 +1,49 @@
 // 1717. Maximum Score From Removing Substrings
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Maximum_Score_From_Removing_Substrings_1717 {
     
 }
 
-class Solution {
+// class Solution {
     
 
-    public int maximumGain(String s, int x, int y) {
-        // remove "ab" gain x
+//     public int maximumGain(String s, int x, int y) {
+//         // remove "ab" gain x
 
-        // remove "ba" gain y
+//         // remove "ba" gain y
 
 
-        return 0;
+//         return 0;
+//     }
+// }
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        HashMap<int[], Double> map = new HashMap<int[], Double>();
+        for (int[] p : points) {
+            map.put(p, Math.sqrt(p[0] * p[0] + p[1] * p[1]));
+        }
+        
+        ArrayList<Double> sortedKeys
+            = new ArrayList<Double>(map.);
+ 
+        Collections.sort(sortedKeys);
+        int i = 0;
+        int[][] ans = new int[k][];
+        
+        // Display the TreeMap which is naturally sorted
+        for (Double x : sortedKeys) {
+            if (i == k) {
+                break;
+            }
+            ans[i] = map.get(x);
+        
+            System.out.println("Key = " + x
+                               + ", Value = " + map.get(x));
+            i++;
+        }
+        return ans;
     }
 }
